659a5d9b7b7bc94b33b8fa6a5978d13e7574ff19
Haebi <haebi@users.noreply.github.com>
2023-04-14 17:14:53 +0900

    Cleaning the Code
    #4.2 Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -7,43 +10,11 @@ const app = express();
 const logger = morgan("dev");
 app.use(logger);
 
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-// const home = (req, res) => {
-//     console.log("I will respond.");
-//     return res.send("hello");
-// };
-
-// const login = (req, res) => {
-//     return res.send("login");
-// };
-
-
-// app.get("/", home);
-// app.get("/login", login);
-
 const handleListening = () => 
-    console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
-
