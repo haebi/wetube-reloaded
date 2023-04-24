0ab839f0593c6714ad80beb427c7c582e12bf983
Haebi <jiggly2k@gmail.com>
2023-04-24 18:18:15 +0900

    Our First Query part Two
    #6.12 Our First Query part Two
    fix home() to use Promise

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,8 +1,22 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home",  });
+// export const home = (req, res) => {
+//   Video.find({}, (error, videos) => { 
+//     return res.render("home", { pageTitle: "Home", videos });
+//   });
+// };
+
+// Mongoose 6.0 버전 이상에서는 Model.find() 함수가 더 이상 콜백 함수를 지원하지 않습니다. 대신에 Model.find()는 Promise를 반환하도록 변경되었습니다.
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  };
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
   res.render("watch", { pageTitle: `Watching` });
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { join, login } from "../controller/userController";
-import { trending, search } from "../controller/videoController";
+import { home, search } from "../controller/videoController";
 
 const globalRouter = express.Router();
 
