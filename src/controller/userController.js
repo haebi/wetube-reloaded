7c96686e6216cd67e04d9e2a3dab64c7db0e9dbb
Haebi <haebi@users.noreply.github.com>
2023-05-12 17:25:36 +0900

    Change Password part One
    #8.4 Change Password part One

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -161,5 +161,15 @@ export const postEdit = async (req, res) => {
   return res.redirect("/users/edit");
 };
 
-export const edit = (req, res) => res.send("Edit User");
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect("/");
+  }
+  return res.render("users/change-password", { pageTitle: "Change Password" });
+};
+export const postChangePassword = (req, res) => {
+  // send notification
+  return res.redirect("/");
+};
+
 export const see = (req, res) => res.send("See User");
