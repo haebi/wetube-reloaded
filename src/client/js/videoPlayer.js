fb7b11cb5f717985f08fc668df19e615b7fde108
Haebi <haebi@users.noreply.github.com>
2023-05-20 17:39:33 +0900

    Duration and Current Time
    #11.4 Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,12 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
-
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
@@ -15,7 +15,6 @@ const handlePlayClick = (e) => {
   }
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-
 const handleMuteClick = (e) => {
   if (video.muted) {
     video.muted = false;
@@ -25,7 +24,6 @@ const handleMuteClick = (e) => {
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
-
 const handleVolumeChange = (event) => {
   const {
     target: { value },
@@ -38,6 +36,16 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currenTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
