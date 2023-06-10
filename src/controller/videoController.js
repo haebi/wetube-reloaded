7e894c9ebec4793765d0dce7beda5abcaa9fbb76
Haebi <haebi@users.noreply.github.com>
2023-06-10 22:12:19 +0900

    Rendering Comments
    #16.6 Rendering Comments

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -8,14 +8,17 @@ export const home = async (req, res) => {
     .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
+
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const {
@@ -31,6 +34,7 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
+
 export const postEdit = async (req, res) => {
   const {
     user: { _id },
@@ -53,9 +57,11 @@ export const postEdit = async (req, res) => {
   req.flash("success", "Changes saved.");
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const {
     user: { _id },
@@ -83,6 +89,7 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   const {
@@ -98,6 +105,7 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
+
 export const search = async (req, res) => {
   const { keyword } = req.query;
   let videos = [];
@@ -110,6 +118,7 @@ export const search = async (req, res) => {
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
+
 export const registerView = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -136,5 +145,7 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
